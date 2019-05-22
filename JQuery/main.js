b01b897098b79f838b4b10b3b85c50830410c9c9
nguyenanh <nguyenanhtran2406@gmail.com>
2019-05-22 10:01:50 +0700

    bi commit

---
+++ JQuery/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $("#hidden").click(function () {
+        $("#text").hide();
+    });
+    $("#show").click(function () {
+        $("#text").show();
+    });
+
+    $("#ani").click(function () {
+       var div = $("#hello");
+       div.animate({left:'500px',fontSize:'100px'},"normal");
+
+    });
+
+});
+
